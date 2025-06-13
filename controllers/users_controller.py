4731d5692b738c71427a6bf0199f616b2658008b
mutuajoseph <wambuajoseph756@gmail.com>
2025-06-13 11:01:49 +0300

    Day 5: Configured db, models, controllers & migrations

---
+++ controllers/users_controller.py
@@ -0,0 +1,49 @@
+from models.user import User, UserType
+from db.database import db
+
+
+def get_all_users():
+    users = db.session.query(User).all()
+    print("USERS", users)
+    return users
+
+
+def get_a_single_user(id: int):
+    user = db.session.query(User).filter_by(User.id == id).first()
+    if user is None:
+        print(f"User with ID {id} does not exist")
+    print("USER", user)
+    return user
+
+
+def create_a_new_user(user: UserType):
+    new_user = User.create(user)
+    print("USER", user)
+    return new_user
+
+
+def get_a_users_and_update(id: int, userObj):
+
+    user = db.session.query(User).filter_by(User.id == id).first()
+
+    if user is None:
+        print(f"User with ID {id} does not exist")
+
+    user["username"] = userObj["username"]
+    user["email"] = userObj['email']
+
+    new_user_info = User.update(user)
+    print("User", new_user_info)
+    return new_user_info
+
+
+def get_a_user_and_delete(id: int):
+
+    user = db.session.query(User).filter_by(User.id == id)
+
+    if user is None:
+        print(f"User with ID {id} does not exist")
+
+    User.delete(user)
+    print(f"User with ID {id} has been deleted successfully")
+    return True
--- main.py
+++ main.py
@@ -1,32 +1,24 @@
-from flask import Flask
-import json
+from flask import Flask, jsonify
+from flask_migrate import Migrate
+from db.config import DATABASE_URL
+from db.database import db
+from controllers import users_controller
 
 # initialize out application instance
 app = Flask(__name__)
 
-my_users = [
-    {
-        "id": 1,
-        "name": "John Kamau",
-        "hobbies": ["Driving", "Swimming", "Listening to Music"]
-    },
-    {
-        "id": 2,
-        "name": "Silvester Mburu",
-        "hobbies": ["Boxing", "Meditating", "Sleeping"]
-    },
-    {
-        "id": 3,
-        "name": "Lynn Kolii",
-        "hobbies": ["Reading Novels", "Shopping", "Socializing"]
-    }
-]
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+db.init_app(app)
+
+migrate = Migrate(app, db)
+
+from models.user import User
 
 
 # declare a route
 @app.route('/')
 def welcome():
-    return "<p>WELCOME TO MY HEALTH E APPLICATION </p>"
+    return jsonify({"message": "WELCOME TO MY HEALTH E APPLICATION", "success": "ok"})
 
 
 @app.route('/users')
@@ -35,42 +27,39 @@ def fetch_all_users():
     List all users
     """
     # convert the my_users to json
-    return json.dumps(my_users)
+    my_users = users_controller.get_all_users()
+    return jsonify(my_users)
 
 
 @app.route('/user/<int:id>')
 def fetch_user(id):
     # filter the list and get the user that matched the id
-    for user in my_users:
-        if user["id"] == id:
-            return json.dumps(user)
-    return {"message": "User Not Found"}
+    user = users_controller.get_a_single_user(id)
+
+    if user:
+        return jsonify(user)
+    return jsonify({"message": "User Not Found"})
 
 
 @app.route('/user/update/<int:id>')
 def update_user(id):
     # filter the list and get the user that matched the id
-    for user in my_users:
-        if user["id"] == id:
-            # UPDATE THE INFORMATION IN THE LIST
-            user["name"] = "John Doe Example"
-            return json.dumps(user)
-    return {"message": "User Not Found"}
+
+    user_obj = {
+        "username": "John Doe Example",
+        "email":"johndoe@gmail.com"
+    }
+
+    user = users_controller.get_a_users_and_update(id, user_obj)
+
+    return jsonify(user)
 
 
 @app.route('/user/delete/<int:id>')
 def delete_user(id):
-
     # filter the list and get the user that matched the id
-    for user in my_users:
-        if user["id"] == id:
-
-            # I do have the user object id, name, hobbies
-            my_users.remove(user)
-
-            return {"message": "User Deleted"}
-    return {"message": "User Not Found"}
-
+    users_controller.get_a_user_and_delete(id)
+    return jsonify({"message": "User Deleted"})
 
 
 # Press the green button in the gutter to run the script.
